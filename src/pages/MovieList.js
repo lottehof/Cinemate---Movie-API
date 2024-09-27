87a67c7e152844090ca62411810f3856262d4b5b
lottehof <s1112052@student.hsleiden.nl>
2024-09-27 20:13:59 +0200

    Complete Files

---
+++ src/pages/MovieList.js
@@ -0,0 +1,21 @@
+import { useFetch } from "../hooks/useFetch";
+import { useEffect } from "react";
+import { Card } from "../components";
+import { useTitle } from "../hooks/useTitle";
+
+export const MovieList = ({apiPath, title}) => {
+  const { data: movies } = useFetch(apiPath);
+  useTitle(`${title}`)
+
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">       
+          { movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          )) }          
+        </div>
+      </section>
+    </main>
+  )
+}
